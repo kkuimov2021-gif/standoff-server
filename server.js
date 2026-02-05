6765cd485476cdc18bafb89973813bdfe83044af
kkuimov2021-gif <kkuimov2021@gmail.com>
2026-02-05 20:25:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Server is working');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
 });
